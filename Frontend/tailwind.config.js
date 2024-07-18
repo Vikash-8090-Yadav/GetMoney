88b6e9a34602c93efa78c116b956d0bc0444f8a4
Vikash <yadav621gzp@gmail.com>
2024-07-18 18:14:01 +0530

    Initialize subgraph

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,26 +1,12 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./Component/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			fontFamily: {
-				'lily-script-one': ['Lily Script One', 'sans-serif'],
-				'Inter': ['Inter','sans-serif'],
-				'Poppins': ['Poppins', 'sans-serif']
-			},
-		},
-	},
-
-	variants: {
-		extend: {
-			display: ["group-focus"],
-			opacity: ["group-focus"],
-			inset: ["group-focus"],
-		},
-	},
-
-	plugins: [require("@tailwindcss/line-clamp")],
-};
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
